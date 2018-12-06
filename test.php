5b8e9838e03f118e1d3c92cd7d7c94e6cee14c5a
takeshikakimoto <takeshi_kakimoto@rudel.jp>
2018-12-06 18:15:08 +0900

    モーダル作るときにテスト用に使ってました

---
+++ test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+    <meta charset = “UFT-8”>
+    <link rel="stylesheet" href="css/test.css">
+    <script type="text/javascript" src="js/test.js"></script>
+    <title>モータルウィンドウ作成</title>
+</head>
+
+<div class="main">
+    <button type="button" id="main_button">モーダルを開くよ！</button>
+    <div class="content1">
+        <textarea></textarea>
+    </div>
+</div>
+
+<div class="modal">
+    <div class="modal_wrapper">
+
+    </div>
+    <div class="modal_content">
+        <p>本当に投稿しますか？</p>
+        <input type="submit" value="投稿する">
+        <button type="button">戻る</button>
+    </div>
+</div>
