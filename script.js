2f6d86acd62c279944e705915eb2fe5c9e330803
takeshikakimoto <takeshi_kakimoto@rudel.jp>
2018-12-07 10:31:17 +0900

    エラー出にくいように入れました

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ window.addEventListener('load', function() {
     const $modal = document.getElementsByClassName('modal');
 
     $button.addEventListener('click', function() {
-        $modal.style.display="block";
+        $modal[1].style.display="block";
     });
 
-    $modal.addEventListener('click', function() {
-        $modal.style.display="none";
+    $modal[1].addEventListener('click', function() {
+        $modal[1].style.display="none";
     });
 
     const windowHeight = window.innerHeight;
-    $modal.style.height = windowHeight + 'px';
+    $modal[1].style.height = windowHeight + 'px';
 
 });
